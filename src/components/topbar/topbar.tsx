18a80a134b28791f58b98f86ecde5a9524498597
winstoniskandar <wpiskandar304@gmail.com>
2022-08-06 11:02:55 -0700

    added functions to top bar

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./topbar.css";
 
 export const topbar = () => {
     return (
     <div className = "topbar">
-       <img className = "back" src = {"../UI/back.png"}/>
-       <img  className = "home" src = {"../UI/home.png"}/>
+        <Link to = "/imageselection"  style={{ color: 'inherit', textDecoration: 'inherit'}}> 
+            <img className = "back" src = {"../UI/back.png"}/>
+        </Link>
+        <Link to = "/"  style={{ color: 'inherit', textDecoration: 'inherit'}}> 
+            <img  className = "home" src = {"../UI/home.png"}/>
+        </Link>
        <img  className = "volumn" src = {"../UI/volumn.png"}/>
+       
     </div>
     );
 }
